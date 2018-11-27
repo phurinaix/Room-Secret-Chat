53710b61a62b719d3687bafe49f002ec456f310e
thirdppp <thirdppp@gmail.com>
2018-11-28 05:27:34 +0700

    auto speak if message provide
    พูดอัตโนมัติ ถ้าได้รับ message

--- public/js/chat.js
+++ public/js/chat.js
@@ -55,9 +55,10 @@ socket.on('newMessage', function(message) {
         text: message.text,
         from: message.from,
         createdAt: formattedTime,
-        speck: message.text
     });
-
+    if(message.text){
+      responsiveVoice.speak("ข้อความจาก"+message.from+"พูดว่า"+message.text,'Thai Male');
+    }
     jQuery('#messages').append(html);
     scrollToBottom();
 });
@@ -103,3 +104,124 @@ locationButton.on('click', function () {
         alert('Unable to fetch locaion.');
     });
 });
+
+//speak sound
+  var msg = new SpeechSynthesisUtterance('สวัสดี, วันนี้ต้องการให้ช่วยอะไร?');
+  msg.lang = 'th-TH';
+  window.speechSynthesis.speak(msg);
+  var final_transcript = '';
+  var recognizing = false;
+  var ignore_onend;
+  if (!('webkitSpeechRecognition' in window)) {
+
+  } else {
+      var recognition = new webkitSpeechRecognition();
+      recognition.lang = "th-TH";
+      recognition.onstart = function() {
+        recognizing = true;
+        start_img.src = 'mic-animate.gif';
+      };
+      recognition.onresult = function(event) {
+        console.log(event)
+        var interim_transcript = '';
+        for (var i = event.resultIndex; i < event.results.length; ++i) {
+          if (event.results[i].isFinal) {
+            final_transcript += event.results[i][0].transcript;
+          } else {
+            interim_transcript += event.results[i][0].transcript;
+          }
+        }
+        final_span.innerHTML = final_transcript;
+        document.getElementById("myText").value = final_span.innerHTML;
+        speak(final_transcript);
+      }
+  }
+  function startButton(event) {
+    if (recognizing) {
+      recognition.stop();
+      return;
+    }
+    final_transcript = '';
+    recognition.start();
+    ignore_onend = false;
+    start_img.src = 'mic-slash.gif';
+  }
+  recognition.onend = function() {
+    recognizing = false;
+    if (ignore_onend) {
+      return;
+    }
+    start_img.src = 'mic.gif';
+  };
+  function speak(text) {
+
+    var msg_light = 'เปิดไฟหน้าบ้าน';
+    var turnoffAllLight = 'ปิดไฟทั้งหมด'
+    var whatsup = 'สบายดีไหม';
+    var nice = 'สบายดี';
+    var sawasdee = 'สวัสดี';
+    var name ='ชื่ออะไร';
+    var fan = 'มีแฟนหรือยัง';
+    var web = 'เว็บนี้ดียังไง';
+    var web1 = 'เว็บไซต์นี้ดียังไง';
+    var web2 = 'เว็บนี้ดีอย่างไง';
+    var webdetail = 'เล่าเกี่ยวกับเว็บนี้';
+   //sitting sound
+    var speechSynthesis = new SpeechSynthesisUtterance();
+    speechSynthesis.voiceURI = 'native';
+    speechSynthesis.volume = 1; // 0 to 1
+    speechSynthesis.rate = 1; // 0.1 to 10
+    speechSynthesis.pitch = 2; //0 to 2
+    speechSynthesis.lang = 'th-TH';
+    var msg = '';
+    //botchat sound
+    if(text.search(msg_light) != -1 || text.search(turnoffAllLight) != -1 ) {
+      msg = "ทำการ" + text + "เรียบร้อยแล้วค่ะ";
+
+      responsiveVoice.speak(msg ,'Thai Female');
+    } else if(text.search(whatsup) != -1) {
+      msg = 'สบายดีค่ะ, แล้วคุณหล่ะคะ ';
+
+      responsiveVoice.speak(msg ,'Thai Female');
+    } else if(text.search(nice) != -1) {
+      msg = 'เรื่องของมึง , บอกกูทำไม';
+
+      responsiveVoice.speak(msg ,'Thai Female');
+    } else if(text.search(sawasdee) != -1) {
+      msg = 'สวัสดีค่ะ,แล้วคุณสบายดีหรือเปล่า';
+
+      responsiveVoice.speak(msg ,'Thai Female');
+    } else if(text.search(name) != -1) {
+      msg = 'ฉันชื่อพิงคุ';
+
+      responsiveVoice.speak(msg ,'Thai Female');
+    } else if(text.search(fan) != -1) {
+      msg = 'มีแล้วชื่อว่าเติ้ดสุดหล่อ';
+
+      responsiveVoice.speak(msg ,'Thai Female');
+    } else if(text.search(web) != -1) {
+      msg = 'เป็น Web Service Chat ที่มีการเข้าระหัสทั้งต้นทางและปลายทางหรือเรียกอีกอย่างนึงคือ End-to-End Encryption หรือ E2EE คือการเข้ารหัสระหว่างคู่สนทนาสองคนตั้งแต่ต้นทางถึงปลายทาง ยกตัวอย่างให้มีคู่สนทนาเป็นนายA และนายBเมื่อนายAส่งข้อความหานายBโดยเปิดระบบE2EE คือLetterSealing ในLINE ข้อความที่ออกไปหานายB จะถูกแปลงเป็นรหัสระหว่างทางโดยมีถนนคืออินเทอร์เน็ตระหว่างที่ข้อความถูกส่งนั้นหากมีผู้ไม่ประสงค์ดีต้องการดูข้อความก็จะแสดงเป็นแค่รหัสเท่านั้นไม่สามารถเห็นข้อความที่นายAส่งถึงนายBได้แบบตรงๆครับ';
+
+      responsiveVoice.speak(msg ,'Thai Female');
+    } else if(text.search(web1) != -1) {
+      msg = 'เป็น Web Service Chat ที่มีการเข้าระหัสทั้งต้นทางและปลายทางหรือเรียกอีกอย่างนึงคือ End-to-End Encryption หรือ E2EE คือการเข้ารหัสระหว่างคู่สนทนาสองคนตั้งแต่ต้นทางถึงปลายทาง ยกตัวอย่างให้มีคู่สนทนาเป็นนายA และนายBเมื่อนายAส่งข้อความหานายBโดยเปิดระบบE2EE คือLetterSealing ในLINE ข้อความที่ออกไปหานายB จะถูกแปลงเป็นรหัสระหว่างทางโดยมีถนนคืออินเทอร์เน็ตระหว่างที่ข้อความถูกส่งนั้นหากมีผู้ไม่ประสงค์ดีต้องการดูข้อความก็จะแสดงเป็นแค่รหัสเท่านั้นไม่สามารถเห็นข้อความที่นายAส่งถึงนายBได้แบบตรงๆครับ';
+
+      responsiveVoice.speak(msg ,'Thai Female');
+    } else if(text.search(web2) != -1) {
+      msg = 'เป็น Web Service Chat ที่มีการเข้าระหัสทั้งต้นทางและปลายทางหรือเรียกอีกอย่างนึงคือ End-to-End Encryption หรือ E2EE คือการเข้ารหัสระหว่างคู่สนทนาสองคนตั้งแต่ต้นทางถึงปลายทาง ยกตัวอย่างให้มีคู่สนทนาเป็นนายA และนายBเมื่อนายAส่งข้อความหานายBโดยเปิดระบบE2EE คือLetterSealing ในLINE ข้อความที่ออกไปหานายB จะถูกแปลงเป็นรหัสระหว่างทางโดยมีถนนคืออินเทอร์เน็ตระหว่างที่ข้อความถูกส่งนั้นหากมีผู้ไม่ประสงค์ดีต้องการดูข้อความก็จะแสดงเป็นแค่รหัสเท่านั้นไม่สามารถเห็นข้อความที่นายAส่งถึงนายBได้แบบตรงๆครับ';
+      speechSynthesis.text = msg;
+      responsiveVoice.speak(msg ,'Thai Female');
+    } else if(text.search(webdetail) != -1) {
+      msg = 'เว็บนี้เป็นเว็บแชทโดยผู้ใช้สามารถสร้างห้องแชทเพื่อใช้พูดคุยกับเพื่อนหรือบุคคลอื่นๆที่ต้องการสนทนาด้วย เว็บนี้จะเป็นช่องทางในการติดต่อสื่อสารซึ่งกันและกัน โดยที่ข้อความที่ผู้ส่งและผู้รับได้รับนั้น จะมั่นใจได้เลยว่าไม่มีการถูกตัดแปลง หรือถูฏแก้ไขได้ หรือจะถูกแอบดักจับข้อความระหว่างทางที่ส่งได้ เพราะข้อความของเว็บไซต์มีการเข้าระหัสแบบRSA จึงมั่นใจได้เลยว่าปลอดภัย มีความเป็นส่วนตัว';
+
+      responsiveVoice.speak(msg ,'Thai Female');
+    }else {
+      msg = 'บทสนทนาที่สามารถคุยกับbotchat ได้ มีดังนี้ '+msg_light+','+turnoffAllLight+','+whatsup+','+sawasdee+','+name+','+fan+','+web+','+web1+','+web2+','+webdetail;
+
+
+
+
+    }
+    window.speechSynthesis.speak(speechSynthesis);
+    final_result.innerHTML = msg;
+  }
--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ io.on('connection', (socket) => {
         users.addUser(socket.id, params.name, params.room);
 
         io.to(params.room).emit('updateUserList', users.getUserList(params.room));
-        socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+        socket.emit('newMessage', generateMessage('Admin', 'welcome to RSA chat'));
         socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
 
         callback();
@@ -54,7 +54,7 @@ io.on('connection', (socket) => {
         users.removeUser(socket.id);
         users.addUser(socket.id, params.name, params.room);
         io.to(params.room).emit('updateUserList', users.getUserList(params.room));
-        socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+        socket.emit('newMessage', generateMessage('Admin', 'welcome to RSA chat'));
         socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
         callback();
     });
